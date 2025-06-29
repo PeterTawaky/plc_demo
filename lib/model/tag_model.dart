966181d8bf4b47f4b13e13219128d1e781d55a7c
Peter Ayman <peterayman800@gmail.com>
2025-06-29 11:30:21 +0300

    prepare snap-7 issu

---
+++ lib/model/tag_model.dart
@@ -0,0 +1,53 @@
+import 'package:excel/excel.dart';
+
+class TagModel {
+  final String tagName;
+  final String type;
+  final String address;
+  final String access;
+  final int acquistion;
+  final bool value;
+  final String description;
+
+  TagModel({
+    required this.tagName,
+    required this.type,
+    required this.address,
+    required this.access,
+    required this.acquistion,
+    required this.value,
+    required this.description,
+  });
+
+  factory TagModel.fromList(List<Data?> data) {
+    return TagModel(
+      tagName: data[0]?.value.toString() ?? '',
+      type: data[1]?.value.toString() ?? '',
+      address: data[2]?.value.toString() ?? '',
+      access: data[3]?.value.toString() ?? '',
+      acquistion: _parseInt(data[4]?.value.toString() ?? ''),
+      value: _parseBool(data[5]?.value.toString() ?? ''),
+      description: data[6]?.value.toString() ?? '',
+    );
+  }
+  // helper method to convert string
+  static bool _parseBool(String value) {
+    if (value == '1') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  static int _parseInt(acquistion) {
+    //split number from the unit
+    RegExp regex = RegExp(r'^(\d+)([a-zA-Z]+)$');
+    Match? match = regex.firstMatch(acquistion);
+    String number = '';
+    if (match != null) {
+      number = match.group(1)!; // "100"
+      String unit = match.group(2)!; // "ms"
+    }
+    return int.parse(number);
+  }
+}
---
+++ lib/service/excel_service.dart
@@ -0,0 +1,33 @@
+import 'dart:developer';
+
+import 'package:excel/excel.dart';
+import 'package:excel_demo/model/tag_model.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+class ExcelService {
+  static Future<List<TagModel>> readExcelData({
+    required String localExcelPath,
+  }) async {
+    try {
+      List<TagModel> tags = [];
+      // Load the Excel file from assets
+      final bytes = await rootBundle.load(localExcelPath);
+      final excel = Excel.decodeBytes(bytes.buffer.asUint8List());
+
+      // Assuming we're reading from the first sheet
+      var sheet = excel.tables[excel.tables.keys.first];
+
+      // Convert sheet data to list of lists
+      // List<List<dynamic>> data = [];
+      for (var row in sheet!.rows.skip(1)) {
+        TagModel tag = TagModel.fromList(row);
+        tags.add(tag);
+      }
+
+      return tags;
+    } catch (e) {
+      log('Error reading Excel file: $e');
+      return [];
+    }
+  }
+}
